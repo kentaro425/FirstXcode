0d2a8691c0c85576d4e21a887b3bbb2e5436d3a9
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-06-03 13:21:11 +0900

    finish

--- FirstXcode/ViewController.swift
+++ FirstXcode/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myLabel: UILabel!
+    
+    @IBAction func buttonTapped(sender: UIButton) {
+        myLabel.text = "Hello World!"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
